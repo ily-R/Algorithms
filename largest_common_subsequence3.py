28832113d7d51b74294428f75d9b200dfd92c58a
ilyas Aroui <arouiilyes@gmail.com>
2018-12-09 23:40:42 +0100

    Update largest_common_subsequence3.py

--- largest_common_subsequence3.py
+++ largest_common_subsequence3.py
@@ -1,45 +1,78 @@
-# use python 3
+# created by ilyas Aroui 20/10/2018
 
 import numpy as np
 
-n = int(input())
-A = [x for x in input().split()]
-m = int(input())
-B = [x for x in input().split()]
-t = int(input())
-C = [x for x in input().split()]
-D = np.zeros((n+1, m+1, t+1), dtype=np.uint64)
 
-
-def longest_subsequence(s1, s2, s3):
-
-    for i in range(1, len(s1)+1):
-        for j in range(1, len(s2)+1):
-            for k in range(1, len(s3) + 1):
-                if s1[i - 1] == s2[j - 1] == s3[k - 1]:
-                    D[i][j][k] = D[i - 1][j - 1][k - 1] + 1
+def longest_subsequence3(n, m, l):
+    """ Fill in the matrix D such that D[i, j, k] is the longest subsequence size between A[0:i], B[0:j] and C[0:k]
+    D, A, B, C are passed to this function by reference.
+        n : the length of the string input A
+        m: the length of the string input B
+        l: the length of the string input C
+    :return:
+        D[n, m, l]: the longest subsequence size between A[0:n], B[0:m] and C[0:l]
+    """
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            for k in range(1, l + 1):
+                if A[i - 1] == B[j - 1] == C[k - 1]:
+                    D[i, j, k] = D[i - 1, j - 1, k - 1] + 1
                 else:
-                    D[i][j][k] = max([D[i - 1][j][k], D[i][j - 1][k], D[i][j][k-1]])
+                    D[i, j, k] = max([D[i - 1, j, k], D[i, j - 1, k], D[i, j, k-1]])
+
+    return D[n, m, l]
 
 
-    return D[len(s1)][len(s2)][len(s3)]
+def output_alignment(i, j, k):
+    """ Backtrack from the the cell D[i, j, k] up to [0, 0, 0] to find the longest common subsequence.
+    :param i: row index in matrix D
+    :param j: column index in matrix D
+    :param k: depth index in matrix D
+    :return:
+        it doesn't return a result. However it fills in the the string common_sequence which is a global variable.
+    """
+    global common_sequence
+    if i == 0 and j == 0:
+        return
+    if i > 0 and D[i, j, k] == D[i-1, j, k]:
+        output_alignment(i - 1, j, k)
+    elif j > 0 and D[i, j, k] == D[i, j - 1, k]:
+        output_alignment(i, j - 1, k)
+    elif k > 0 and D[i, j, k] == D[i, j, k - 1]:
+        output_alignment(i, j, k - 1)
+    else:
+        output_alignment(i-1, j-1, k-1)
+        if i >= j and i >= k:
+            common_sequence += A[i-1]
+        elif j >= i and j >= k:
+            common_sequence += B[j-1]
+        else:
+            common_sequence += C[k-1]
 
-# commun_sequence = ''
-# def output_alignment(i, j):
-#     global  commun_sequence
-#     if i == 0 and j == 0:
-#         return
-#     if i > 0 and D[i][j] == D[i-1][j]:
-#         output_alignment(i - 1, j)
-#     elif j > 0 and D[i][j] == D[i][j - 1]:
-#         output_alignment(i, j - 1)
-#     else:
-#         output_alignment(i-1, j-1)
-#         if i >=j:
-#             commun_sequence += A[i - 1]
-#         else:
-#             commun_sequence += A[j - 1]
-#
+
+def read_data():
+    """
+    :return:
+    n: length of sequence1
+    m: length of sequence2
+    sequence1: a string input
+    sequence2: a string input
+    sequence3: a string input
+    """
+    sequence1 = input()
+    sequence2 = input()
+    sequence3 = input()
+    n = len(sequence1)
+    m = len(sequence2)
+    l = len(sequence3)
+    return n, m, l, sequence1, sequence2, sequence3
 
 
-print(longest_subsequence(A, B, C))
+n, m, l, A, B, C = read_data()
+common_sequence = ''
+D = np.zeros((n + 1, m + 1, l + 1), dtype=np.uint64)
+longest_sequence_size = longest_subsequence3(n, m, l)
+output_alignment(n, m, l)
+
+print(longest_sequence_size)
+print(common_sequence)
